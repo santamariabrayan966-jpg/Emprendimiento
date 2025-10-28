e60fc40d4ec121745956574009b3cbe376534b88
santamariabrayan966-jpg <santamariabrayan966@gmail.com>
2025-10-28 01:04:22 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Efecto suave de entrada
-document.addEventListener('DOMContentLoaded', () => {
-  const sections = document.querySelectorAll('.hero, .subhero, .contenido');
-  sections.forEach(sec => {
-    sec.style.opacity = '0';
-    sec.style.transition = 'opacity 1.2s ease';
-    setTimeout(() => sec.style.opacity = '1', 300);
-  });
-});
